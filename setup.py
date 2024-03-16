ed8443e45c797bf023f8cb8ecdabb09a75994c6a
hariomloharapps <163524293+hariomloharapps@users.noreply.github.com>
2024-03-16 14:56:59 +0530

    Update ttsetup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+
+
+
+
 setup(
     name='kvdroid',
     packages=['kvdroid'],
